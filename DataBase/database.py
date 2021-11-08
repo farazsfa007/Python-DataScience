2a43574c5b6f31b196d71d389748f8d3b1391206
farazsfa007 <farazahmadsfa@gmail.com>
2021-11-08 19:39:12 +0530

    ..

--- DataBase/database.py
+++ DataBase/database.py
@@ -1,18 +1,21 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column,Integer, String,create_engine
+from sqlalchemy import Column, Integer, String, create_engine
 
-Base=declarative_base()
+Base = declarative_base()
 
-class Smartphone(Base):
-    __tablename__='smartphone'
 
-    id=Column(Integer,primary_key=True,autoincrement=True )
-    brand=Column(String)
-    name=Column(String)
-    price=Column(Integer)
-    ram=Column(Integer)
-    storage=Column(Integer)
+class SmartPhone(Base):
 
-if __name__=='__main__':
-    engine=create_engine('sqlite:///mydatabase.sqlite3')
+    __tablename__ = "smartphones"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    brand = Column(String)
+    name = Column(String)
+    price = Column(Integer)
+    ram = Column(Integer)
+    storage = Column(Integer)
+
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///mydatabase.sqlite3")
     Base.metadata.create_all(engine)
